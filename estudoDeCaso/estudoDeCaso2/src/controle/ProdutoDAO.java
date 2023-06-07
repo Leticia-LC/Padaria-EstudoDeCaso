22b4a886f69ec0cce8f5d9c7ba196dc3f7306f35
Kemily Cristina Schmidt de Oliveira <kemily.c2006@aluno.ifsc.edu.br>
2023-06-07 17:18:08 -0300

    Menu

--- estudoDeCaso/estudoDeCaso2/src/controle/ProdutoDAO.java
+++ estudoDeCaso/estudoDeCaso2/src/controle/ProdutoDAO.java
@@ -20,9 +20,21 @@ public class ProdutoDAO implements IProdutoDAO {
 			
 			// produtos pre cadastrados
 			
-			Produto p = new Produto();
-			p.setNomeProduto("Pastel");
-			produtos.add(p);
+			Produto p1 = new Produto();
+			p1.setNomeProduto("Pastel de carne com queijo cheddar");
+			p1.setValorCompra(8.50);
+			p1.setCodigoProduto(23456787);
+			produtos.add(p1);
+			Produto p2 = new Produto();
+			p2.setNomeProduto("Torta mineira de abacaxi e coco");
+			p2.setValorCompra(45.00);
+			p2.setCodigoProduto(23456433);
+			produtos.add(p2);
+			Produto p3 = new Produto();
+			p3.setNomeProduto("Bolo RedVelvet com geleia de frutas vermelhas");
+			p3.setValorCompra(60.50);
+			p3.setCodigoProduto(468434566);
+			
 		}
 
 		return instancia;
